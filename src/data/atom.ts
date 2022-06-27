a7477129cd419cff26c775eed9fa151f24b974d5
corgi-world <developer.msw@gmail.com>
2022-06-27 14:22:41 +0900

    전역 상태 관리 atom 추가

---
+++ src/data/atom.ts
@@ -0,0 +1,78 @@
+import { atom, selectorFamily } from "recoil";
+
+import { iTopic } from "./api";
+
+export const topicAtom = atom<iTopic[]>({
+  key: "topicAtom",
+  default: [],
+});
+
+export const genderSelector = selectorFamily({
+  key: "genderSelector",
+  get:
+    (targetKeyword) =>
+    ({ get }) => {
+      const topic = get(topicAtom);
+      const { male, female } = topic.filter(
+        ({ keyword }) => targetKeyword === keyword
+      )[0];
+      return [male, female];
+    },
+});
+
+export const ageSelector = selectorFamily({
+  key: "ageSelector",
+  get:
+    (targetKeyword) =>
+    ({ get }) => {
+      const topic = get(topicAtom);
+      const { age_10, age_20, age_30, age_40, age_50, age_60 } = topic.filter(
+        ({ keyword }) => targetKeyword === keyword
+      )[0];
+      return [age_10, age_20, age_30, age_40, age_50, age_60];
+    },
+});
+
+export const linkSelector = selectorFamily({
+  key: "linkSelector",
+  get:
+    (targetKeyword) =>
+    ({ get }) => {
+      const topic = get(topicAtom);
+      const { rankurl } = topic.filter(({ keyword }) => targetKeyword === keyword)[0];
+      return rankurl;
+    },
+});
+
+export const summarySelector = selectorFamily({
+  key: "summarySelector",
+  get:
+    (targetKeyword) =>
+    ({ get }) => {
+      const topic = get(topicAtom);
+      const { summary } = topic.filter(({ keyword }) => targetKeyword === keyword)[0];
+      return summary;
+    },
+});
+
+export const relatedKeywordSelector = selectorFamily({
+  key: "relatedKeywordSelector",
+  get:
+    (targetKeyword) =>
+    ({ get }) => {
+      const topic = get(topicAtom);
+      const targetTopic = topic.filter(({ keyword }) => targetKeyword === keyword)[0];
+
+      const result = [];
+      for (const key in targetTopic) {
+        if (key.includes("search")) {
+          const related = targetTopic[key];
+          if (related !== "0") {
+            result.push(related);
+          }
+        }
+      }
+
+      return result;
+    },
+});
